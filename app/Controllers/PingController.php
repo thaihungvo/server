e997410d92fe1ec11d6d36b91100cbcaf83809b9
Cristian Barlutiu <cristian.barlutiu@gmail.com>
2020-10-28 14:26:51 +0100

    docker file reorder

--- app/Controllers/PingController.php
+++
@@ -1,9 +0,0 @@
-<?php namespace App\Controllers;
-
-class PingController extends BaseController
-{
-	public function index()
-	{
-        return $this->reply("pong", 200);
-	}
-}
--- app/Models/HomeModel.php
+++
@@ -1,8 +0,0 @@
-<?php namespace App\Models;
-
-use CodeIgniter\Model;
-
-class HomeModel extends Model
-{
-    
-}
