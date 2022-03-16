1938670b0674d281165b393384795a732ea8c53e
Cristian Barlutiu <cristian.barlutiu@gmail.com>
2022-03-16 23:37:46 +0100

    added nano id

--- www/app/Config/Autoload.php
+++ www/app/Config/Autoload.php
@@ -53,7 +53,8 @@ class Autoload extends \CodeIgniter\Config\AutoloadConfig
 			'App'         => APPPATH,                // To ensure filters, etc still found,
 			APP_NAMESPACE => APPPATH,                // For custom namespace
             'Config'      => APPPATH . 'Config',
-            'Firebase'    => APPPATH . 'ThirdParty/Firebase'
+            'Firebase'    => APPPATH . 'ThirdParty/Firebase',
+            'Hidehalo'      => APPPATH . 'ThirdParty/Hidehalo',
 		];
 
 		/**
